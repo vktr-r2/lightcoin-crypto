fa41a9499c57ecd3eac3c23f26e75372215e6a96
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-16 11:49:14 -0400

    Implement setters to set value for withdrawal or deposit.  commit() now implemented in superclass Transaction

--- index.js
+++ index.js
@@ -1,29 +1,70 @@
-let balance = 500.00;
+// Here's a list of features that our code needs to support:
 
-class Withdrawal {
+// Allow multiple accounts to be created  >> create a method
+// Each account can have many transactions >> create a method
+// Allow withdrawals and deposits into accounts >> create a method
+// Allow us to retrieve the transaction history of an account (all withdrawals and deposits) >> create a method
+// Allow us to retrieve the current balance of the account at any time >> create a method
+// Don't allow withdrawals that exceed the remaining balance of the account >> create a method
 
-  constructor(amount) {
+
+
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.value;
   }
 
 }
 
 
 
+class Deposit extends Transaction {
+
+  get value () {
+    return this.amount;
+  }
+
+}
+
+
+
+class Withdrawal extends Transaction {
+
+  get value () {
+    return -this.amount;
+  }
+
+}
+
+
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    //Account balance starts at zero when introduced
+    this.balance = 0;
+  }
+}
+
+
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("Chequing");
+
+console.log("Starting Balance:", myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log('Ending Balance:', myAccount.balance);
